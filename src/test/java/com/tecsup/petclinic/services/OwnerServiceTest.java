8e697655e4edef8d976d943ccdd721f12ce49426
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-29 17:42:54 -0500

    correcion

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,5 +1,8 @@
 package com.tecsup.petclinic.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +12,6 @@ import com.tecsup.petclinic.entities.Owner;
 import com.tecsup.petclinic.exceptions.OwnerNotFoundException;
 
 import lombok.extern.slf4j.Slf4j;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @Slf4j
 public class OwnerServiceTest {
@@ -22,10 +22,10 @@ public class OwnerServiceTest {
 	@Test
 	public void testFindOwnerById() {
 
-		Integer ID = 2;
+		Integer ID = 1;
 		String NAME = "Betty";
 		Owner owner = null;
-		
+
 		try {
 			owner = ownerService.findById(ID);
 
@@ -36,34 +36,6 @@ public class OwnerServiceTest {
 
 		assertEquals(NAME, owner.getFirstName());
 
-	}
-	@Test
-	public void testCreateOwner() {
-
-		String FIRST_NAME = "jurgen";
-		String LAST_NAME = "calle";
-		String ADDRESS = "Av.las flores";
-		String CITY = "Cuzco";
-		String TELEPHONE = "987600243";
-
-		Owner owner = new Owner(FIRST_NAME, LAST_NAME, ADDRESS, CITY, TELEPHONE);
-
-		Owner ownerCreated = ownerService.create(owner);
-
-		log.info("Owner created: " + ownerCreated);
-
-		assertNotNull(ownerCreated.getId(), "El ID no debe ser null");
-		assertEquals(FIRST_NAME, ownerCreated.getFirstName());
-		assertEquals(LAST_NAME, ownerCreated.getLastName());
-		assertEquals(ADDRESS, ownerCreated.getAddress());
-		assertEquals(CITY, ownerCreated.getCity());
-		assertEquals(TELEPHONE, ownerCreated.getTelephone());
-
-		try {
-			ownerService.delete(ownerCreated.getId());
-		} catch (OwnerNotFoundException e) {
-			fail("No se pudo eliminar el owner de prueba: " + e.getMessage());
-		}
 	}
 
 	@Test
