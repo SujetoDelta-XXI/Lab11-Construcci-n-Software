162c994c2c337225afcda6438f1abd7712ff6419
Angeela-Lopeez <angela.lopez@tecsup.edu.pe>
2025-06-05 17:52:17 -0500

    terminade buscar por id

--- src/test/java/com/tecsup/petclinic/webs/Lopez.java
+++ src/test/java/com/tecsup/petclinic/webs/Lopez.java
@@ -1,30 +1,58 @@
 package com.tecsup.petclinic.webs;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper; //Json a objeto
+import com.tecsup.petclinic.dtos.OwnerDTO; //objeto - dueño
+import com.tecsup.petclinic.repositories.OwnerRepository; //acceso bd
+
+import lombok.extern.slf4j.Slf4j; //soporte a log.info
+import org.junit.jupiter.api.Test; //prueba automatizada
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc //simular peticiones http
+@Slf4j //habilita logg para imprimir mensajes
 public class Lopez {
 
     @Autowired
-    private MockMvc mockMvc;
-
+    private MockMvc mockMvc;   //simular peticiones
     @Autowired
-    private ObjectMapper objectMapper;
-
+    private ObjectMapper objectMapper;  //obj json en class
+    @Autowired
+    private OwnerRepository ownerRepository; //accder bd
     @Test
-    public void testFindOwnerById() throws Exception {
-        mockMvc.perform(get("/owners/3") // ID existente
+    void FindOwnerById() throws Exception { //metod
+        int id = 2;  //ID conocido en la bd
+
+        //ejecuta get - id 2
+        MvcResult result = mockMvc.perform(get("/owners/" + id)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()) //verifica
-                .andExpect(jsonPath("$.firstName").value("Eduardo")); //verifica el nombre
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is("Betty")))
+                .andReturn();
+
+        String responseJson = result.getResponse().getContentAsString();
+        log.info("Respuesta JSON del GET: {}", responseJson);
+
+        //validar
+        OwnerDTO owner = objectMapper.readValue(responseJson, OwnerDTO.class);
+
+        assertEquals("Betty", owner.getFirstName());
+        assertEquals("Davis", owner.getLastName());
+        log.info("Owner verificado correctamente");
     }
 }
+
+
+
